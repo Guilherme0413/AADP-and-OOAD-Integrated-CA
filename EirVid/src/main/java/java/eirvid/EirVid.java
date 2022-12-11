04b710edbfa6fd7b0a6dcc4b753a15d97610b98d
Danrlei Martins <danrlei.martins@hotmail.com>
2022-12-11 18:49:41 +0000

    1st Commit, java project created

---
+++ EirVid/src/main/java/java/eirvid/EirVid.java
@@ -0,0 +1,17 @@
+/*
+Algorithms, Architecture and Design Patterns
+Object Oriented Analysis and Design
+Integrated Continuous Assessment 3
+*/
+package java.eirvid;
+
+/**
+ *
+ * @author Danrlei Martins
+ */
+public class EirVid {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to ÉirVid Movie Rentals!");
+    }
+}
