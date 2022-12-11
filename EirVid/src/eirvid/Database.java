3a46406715d06363dbc7bc206a898bd7e349b884
Danrlei Martins <danrlei.martins@hotmail.com>
2022-12-11 19:52:38 +0000

    java project created
    main class, database class, added mysql connector

---
+++ EirVid/src/eirvid/Database.java
@@ -0,0 +1,14 @@
+/*
+Algorithms, Architecture and Design Patterns
+Object Oriented Analysis and Design
+Integrated Continuous Assessment 3
+*/
+package eirvid;
+
+/**
+ *
+ * @author Danrlei Martins
+ */
+public class Database {
+    
+}
---
+++ EirVid/src/eirvid/EirVid.java
@@ -0,0 +1,21 @@
+/*
+Algorithms, Architecture and Design Patterns
+Object Oriented Analysis and Design
+Integrated Continuous Assessment 3
+*/
+package eirvid;
+
+/**
+ *
+ * @author Danrlei Martins
+ */
+public class EirVid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
